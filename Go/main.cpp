3ba4672dcc63b9e3d35b250c3546a0c90d28cedb
Ellis Hoag <eshoag2@illinois.edu>
2017-04-04 21:01:39 -0500

    Initial Commit

---
+++ Go/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Go
+//
+//  Created by Ellis Hoag on 4/4/17.
+//  Copyright © 2017 Ellis Hoag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
